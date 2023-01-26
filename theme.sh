0fc435547368241eec5d5ca078d868b918b6757d
sreenathachakala <104345837+sreenathachakala@users.noreply.github.com>
2023-01-26 21:03:35 +0530

    saving changes

--- theme.sh
+++ theme.sh
@@ -11,6 +11,7 @@ chown -R root:root /mate-theme/theme/$MATE_THEME_DARK
 cp -r /mate-theme/theme/$MATE_THEME /usr/share/themes/$MATE_THEME
 cp -r /mate-theme/theme/$MATE_THEME_DARK /usr/share/themes/$MATE_THEME_DARK
 cp -r /mate-theme/icon-theme/$MATE_ICON_THEME /usr/share/icons/$MATE_ICON_THEME
+cp -r /mate-theme/icon-theme/$MATE_ICON_THEME_DARK /usr/share/icons/$MATE_ICON_THEME_DARK
 cp /mate-theme/theme/ubuntu-mate-custom.xml /usr/share/mate-background-properties/ubuntu-mate-custom.xml
 cp /mate-theme/theme/$MATE_IMAGE /usr/share/backgrounds/$MATE_IMAGE
 cp /mate-theme/theme/zzz.gschema.override /usr/share/glib-2.0/schemas/zzz.gschema.override
@@ -68,6 +69,7 @@ sed -i 's/MATE_IMAGE/'"$MATE_IMAGE"'/' /usr/share/mate-background-properties/ubu
 
 sed -i 's/MATE_THEME/'"$MATE_THEME"'/' /usr/share/glib-2.0/schemas/zzz.gschema.override
 sed -i 's/MATE_IMAGE/'"$MATE_IMAGE"'/' /usr/share/glib-2.0/schemas/zzz.gschema.override
+sed -i 's/MATE_ICON_THEME/'"$MATE_ICON_THEME"'/' /usr/share/glib-2.0/schemas/zzz.gschema.override
 
 chown root:root /usr/share/mate-background-properties/ubuntu-mate-custom.xml
 chown root:root /usr/share/backgrounds/$MATE_IMAGE
@@ -75,7 +77,7 @@ chown root:root /usr/share/glib-2.0/schemas/zzz.gschema.override
 chmod 777 /usr/share/backgrounds/$MATE_IMAGE
 chmod 777 /usr/share/mate-background-properties/ubuntu-mate-custom.xml
 chmod 777 /usr/share/glib-2.0/schemas/zzz.gschema.override
-# chmod 777 /usr/share/mate-panel/layouts/custom-mate.layout
+chmod 777 /usr/share/mate-panel/layouts/custom-mate.layout
 
 ls -l /usr/share/mate-panel/layouts/
 cd /usr/share/glib-2.0/schemas && glib-compile-schemas /usr/share/glib-2.0/schemas
