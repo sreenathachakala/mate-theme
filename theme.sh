2db793c0224d307a25cf44eaf6e8b86f2e0e4bc3
sreenathachakala <104345837+sreenathachakala@users.noreply.github.com>
2023-01-29 22:14:29 +0530

    changes to the theme

--- theme.sh
+++ theme.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-export MATE_THEME=WhiteSur-Dark
-export MATE_THEME_DARK=WhiteSur-Light
-export MATE_ICON_THEME=WhiteSur-dark
-export MATE_ICON_THEME_DARK=WhiteSur
+export MATE_THEME=WhiteSur-Light
+export MATE_THEME_DARK=WhiteSur-Dark
+export MATE_ICON_THEME=WhiteSur
+export MATE_ICON_THEME_DARK=WhiteSur-dark
 export MATE_IMAGE=Black.jpg
 export FONTFILE=IBM_Plex_Sans
 export FONT_NAME='IBM Plex Sans 11'
 export TITLEBAR_FONTNAME='IBM Plex Sans Medium 11'
+export DOCK_THEME='mcOS-BS-iMacM1-Black'
 
 cd /mate-theme/theme/ && tar -xf $MATE_THEME.tar.xz
 cd /mate-theme/theme/ && tar -xf $MATE_THEME_DARK.tar.xz
 cd /mate-theme/icon-theme && tar -xf $MATE_ICON_THEME.tar.xz
 cd /mate-theme/fonts && unzip $FONTFILE.zip -d $FONTFILE
 
-ls /mate-theme/theme
-ls /mate-theme/icon-theme
-ls /mate-theme/fonts
+git clone https://github.com/hamburgerghini1/macos-plank-themes.git
+cp -r .local/share/plank/themes/ /usr/share/plank/themes/
 
 chown -R root:root /mate-theme/theme/$MATE_THEME
 chown -R root:root /mate-theme/theme/$MATE_THEME_DARK
@@ -40,39 +40,39 @@ cp -a /mate-theme/backgrounds/. /usr/share/backgrounds/ubuntu-mate-common/
 #  /usr/share/icons/$MATE_ICON_THEME/places/*/start-here-symbolic.svg \
 #  /usr/share/icons/$MATE_ICON_THEME/status/*/start-here-symbolic.svg
 
-cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places/16/start-here.svg
-cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places/22/start-here.svg
-cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places/24/start-here.svg
-cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places/scalable/start-here.svg
-cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places/symbolic/start-here-symbolic.svg
+# cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places/16/start-here.svg
+# cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places/22/start-here.svg
+# cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places/24/start-here.svg
+# cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places/scalable/start-here.svg
+# cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places/symbolic/start-here-symbolic.svg
 
-cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places@2x/16/start-here.svg
-cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places@2x/22/start-here.svg
-cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places@2x/24/start-here.svg
-cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places@2x/scalable/start-here.svg
-cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places@2x/symbolic/start-here-symbolic.svg
+# cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places@2x/16/start-here.svg
+# cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places@2x/22/start-here.svg
+# cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places@2x/24/start-here.svg
+# cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places@2x/scalable/start-here.svg
+# cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/places@2x/symbolic/start-here-symbolic.svg
 
-cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/status/16/start-here-symbolic.svg
-cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/status/22/start-here-symbolic.svg
-cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/status/24/start-here-symbolic.svg
-cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/status/symbolic/start-here-symbolic.svg
+# cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/status/16/start-here-symbolic.svg
+# cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/status/22/start-here-symbolic.svg
+# cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/status/24/start-here-symbolic.svg
+# cp /mate-theme/icon-theme/start-here.svg /usr/share/icons/$MATE_ICON_THEME/status/symbolic/start-here-symbolic.svg
 
-chmod 777 /usr/share/icons/$MATE_ICON_THEME/places/16/start-here.svg \
- /usr/share/icons/$MATE_ICON_THEME/places/22/start-here.svg \
- /usr/share/icons/$MATE_ICON_THEME/places/24/start-here.svg \
- /usr/share/icons/$MATE_ICON_THEME/places/scalable/start-here.svg \
- /usr/share/icons/$MATE_ICON_THEME/places/symbolic/start-here-symbolic.svg
+# chmod 777 /usr/share/icons/$MATE_ICON_THEME/places/16/start-here.svg \
+#  /usr/share/icons/$MATE_ICON_THEME/places/22/start-here.svg \
+#  /usr/share/icons/$MATE_ICON_THEME/places/24/start-here.svg \
+#  /usr/share/icons/$MATE_ICON_THEME/places/scalable/start-here.svg \
+#  /usr/share/icons/$MATE_ICON_THEME/places/symbolic/start-here-symbolic.svg
 
-chmod 777 /usr/share/icons/$MATE_ICON_THEME/places@2x/16/start-here.svg \
- /usr/share/icons/$MATE_ICON_THEME/places@2x/22/start-here.svg \
- /usr/share/icons/$MATE_ICON_THEME/places@2x/24/start-here.svg \
- /usr/share/icons/$MATE_ICON_THEME/places@2x/scalable/start-here.svg \
- /usr/share/icons/$MATE_ICON_THEME/places@2x/symbolic/start-here-symbolic.svg
+# chmod 777 /usr/share/icons/$MATE_ICON_THEME/places@2x/16/start-here.svg \
+#  /usr/share/icons/$MATE_ICON_THEME/places@2x/22/start-here.svg \
+#  /usr/share/icons/$MATE_ICON_THEME/places@2x/24/start-here.svg \
+#  /usr/share/icons/$MATE_ICON_THEME/places@2x/scalable/start-here.svg \
+#  /usr/share/icons/$MATE_ICON_THEME/places@2x/symbolic/start-here-symbolic.svg
 
- chmod 777 /usr/share/icons/$MATE_ICON_THEME/status/16/start-here-symbolic.svg \
- /usr/share/icons/$MATE_ICON_THEME/status/22/start-here-symbolic.svg \
- /usr/share/icons/$MATE_ICON_THEME/status/24/start-here-symbolic.svg \
- /usr/share/icons/$MATE_ICON_THEME/status/symbolic/start-here-symbolic.svg
+#  chmod 777 /usr/share/icons/$MATE_ICON_THEME/status/16/start-here-symbolic.svg \
+#  /usr/share/icons/$MATE_ICON_THEME/status/22/start-here-symbolic.svg \
+#  /usr/share/icons/$MATE_ICON_THEME/status/24/start-here-symbolic.svg \
+#  /usr/share/icons/$MATE_ICON_THEME/status/symbolic/start-here-symbolic.svg
 
 
 sed -i 's/MATE_THEME/'"$MATE_THEME"'/' /usr/share/mate-background-properties/ubuntu-mate-custom.xml
@@ -83,6 +83,7 @@ sed -i 's/MATE_IMAGE/'"$MATE_IMAGE"'/' /usr/share/glib-2.0/schemas/zzz.gschema.o
 sed -i 's/MATE_ICON_THEME/'"$MATE_ICON_THEME"'/' /usr/share/glib-2.0/schemas/zzz.gschema.override
 sed -i 's/FONT_NAME/'"$FONT_NAME"'/' /usr/share/glib-2.0/schemas/zzz.gschema.override
 sed -i 's/TITLEBAR_FONTNAME/'"$TITLEBAR_FONTNAME"'/' /usr/share/glib-2.0/schemas/zzz.gschema.override
+sed -i 's/DOCK_THEME/'"$DOCK_THEME"'/' /usr/share/glib-2.0/schemas/zzz.gschema.override
 
 
 chown root:root /usr/share/mate-background-properties/ubuntu-mate-custom.xml
